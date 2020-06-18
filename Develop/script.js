506370e99032f2785848390356d5474c1dae9671
Red Dragon <carsonkeller1995@hotmail.com>
2020-06-18 17:02:39 -0500

    Completed HW assignment of the Random Password Generator. Code is still kind of a mess but it functions

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,97 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
+var charlength;
+var uppercase;
+var lowercase;
+var numerals;
+var specials;
+var starter;
+var confirm_charlength;
+var lowercasechars = 'abcdefghijklmnopqrstuvwxyz';
+var uppercasechars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numeralschars = '0123456789';
+var specialschars = '!@#$%^&*()_+{}:"<>?\|[];\',./`~';
+var possible_selection = "";
+var password = "";
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function numberOfCharacters(length) {
+
+  console.log(length)
+}
+numberOfCharacters(prompt)
+function writePassword() {
+  possible_selection = "";
+  password = "";
+  var txt;
+  var starter = confirm("Hello! Would you like to generate a random password?");
+  if (starter == true) {
+    txt = "You have begun the process of getting a new password!";
+  } else {
+    txt = "You pressed Cancel!";
+  }
+
+  console.log(starter)
+
+  charlength = prompt("Please indicate how long you would like your password to be, it must be between 8 and 128 characters.", "128");
+  if (charlength == null || charlength == "") {
+    txt = "User cancelled the prompt.";
+  } else {
+    txt = "Your password will be " + charlength + "characters long.";
+    confirm_charlength = charlength
+  };
+
+  console.log(charlength)
+
+  uppercase = prompt("Would you like uppercase letters in your password?", "Click yes if you would like them, hit cancel if you don't.");
+  if (uppercase == null || uppercase == "") {
+    txt = "You will not have uppercase letters in your password";
+  } else {
+    txt = "You will have uppercase letters in your password";
+    uppercase = true;
+    possible_selection = possible_selection + uppercasechars
+  }
+
+  console.log(txt)
+
+  lowercase = prompt("Would you like lowercase letters in your password?", "Click yes if you would like them, hit cancel if you don't.");
+  if (lowercase == null || lowercase == "") {
+    txt = "You will not have lowercase letters in your password";
+  } else {
+    txt = "You will have lowercase letters in your password";
+    lowercase = true;
+    possible_selection = possible_selection + lowercasechars
+  }
+  console.log(txt)
+
+  numerals = prompt("Would you like numbers in your password?", "Click yes if you would like them, hit cancel if you don't.");
+  if (numerals == null || numerals == "") {
+    txt = "You will not have numbers in your password";
+  } else {
+    txt = "You will have numbers in your password";
+    numerals = true;
+    possible_selection = possible_selection + numeralschars
+  }
+  console.log(txt)
+
+  specials = prompt("Would you like special characters in your password?", "Click yes if you would like them, hit cancel if you don't.");
+  if (specials == null || specials == "") {
+    txt = "You will not have special characters in your password";
+  } else {
+    txt = "You will have special characters in your password";
+    specials = true;
+    possible_selection = possible_selection + specialschars
+  }
+  console.log(possible_selection)
+  for (var i = 0; i < charlength; i++) {
+    console.log(possible_selection[Math.floor(Math.random() * possible_selection.length)])
+    password = password + possible_selection[Math.floor(Math.random() * possible_selection.length)]
+  }
+  console.log(password)
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+};
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Random-Password-Generator-CK
+
+In this homework I was required to make a random password generator.
+
+I did this by creating a series of prompts that asked the user whether or not they:
+
+A) Wanted to create a password
+B) How long they wanted the password to be
+C) Whether or not they wanted uppercase letters in the password
+D) Whether or not they wanted lowercase letters in the password
+E) Whether or not they wanted numbers in their password
+F) Whether or not they wanted special characters in their password
+
+Within these prompts were global variables that would come into play by the end of the password generator. The global variable possible_selection, which was added to the value of the prompts in order to see whether or not the variable in question was to be added to the password.
+
+The password generator itself involves a for loop and includes other global variables whose values were the numbers/letters/characters corresponding to uppercase, lowercase, numbers, and special characters. 
+
+At this point the password variable comes into play and its value was itself plus the possible_selection (which by this point had determined which variables were to be included), a math randomizer, and the length of the possible selection. The end result is the password as it appears on screen.
